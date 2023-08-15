d2ea83c770830bf148baf9887dd68e4eae7fb56c
zainuull <thobazainul@gmail.com>
2023-08-15 17:29:53 +0700

    hello

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl font-semibold">Hello</h1>
+      <Button/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <div>
+      <button className="px-6 py-2 text-xl font-semibold rounded-lg bg-black text-white">Click Me</button>
+    </div>
+  )
+}
+
+export default Button
